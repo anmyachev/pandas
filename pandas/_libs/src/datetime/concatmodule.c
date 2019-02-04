535696fa2cb9db2e4a2551d2b11a2c55a7b5cc2a
Vasily Litvinov <vasilij.n.litvinov@intel.com>
2019-02-04 16:30:05 +0300

    Add fast path for sequence of numpy arrays

--- pandas/_libs/src/datetime/concatmodule.c
+++ pandas/_libs/src/datetime/concatmodule.c
@@ -31,6 +31,13 @@ static inline int convert_and_set_item(PyObject *item, Py_ssize_t index, PyArray
     return 1;
 }
 
+static PyObject* free_arrays(PyObject** arrays, Py_ssize_t size) {
+    PyObject** item = arrays;
+    for (Py_ssize_t i = 0; i < size; ++i, ++item) Py_DECREF(*item);
+    free(arrays);
+    return NULL;
+}
+
 static PyObject*
 concat_date_cols(PyObject *self, PyObject *args)
 {
@@ -108,85 +115,108 @@ concat_date_cols(PyObject *self, PyObject *args)
     } else {
         PyObject **arrays = (PyObject**) malloc(sizeof(PyObject*) * sequence_size);
         PyObject* array = NULL;
+        PyObject** parray = NULL;
         PyObject* fast_array = NULL;
         Py_ssize_t min_array_size = 0;
+        int all_numpy = 1;
         for (Py_ssize_t i = 0; i < sequence_size; ++i) {
             array = PySequence_GetItem(sequence, i);
             if (array == NULL) {
-                for (Py_ssize_t j = 0; j < i; ++j) Py_DECREF(arrays[j]);
-                free(arrays);
-                return NULL;
+                return free_arrays(arrays, i);
             }
-            fast_array = PySequence_Fast(array, "elements of input sequence must be sequence");
-            if (fast_array == NULL) {
-                Py_DECREF(array);
-                for (Py_ssize_t j = 0; j < i; ++j) Py_DECREF(arrays[j]);
-                free(arrays);
-                return NULL;  //PySequence_Fast set message, which in second argument
-            }
-            Py_DECREF(array);
-
-            Py_ssize_t array_size = PySequence_Fast_GET_SIZE(fast_array);
-            if (min_array_size != 0) {
-                min_array_size = (array_size < min_array_size) ? array_size : min_array_size;
+            if (PyArray_CheckExact(array)) {
+                if (PyArray_NDIM((PyArrayObject*)array) != 1) {
+                    PyErr_SetString(PyExc_ValueError, "ndarrays must be 1-dimentional");
+                    return free_arrays(arrays, i);
+                }
             } else {
-                min_array_size = array_size;
+                all_numpy = 0;
             }
-            arrays[i]  = fast_array;
+            arrays[i] = array;
+        }
+
+        parray = arrays;
+        if (all_numpy) {
+            for (Py_ssize_t i = 0; i < sequence_size; ++i, ++parray) {
+                Py_ssize_t array_size = PyArray_SIZE((PyArrayObject*)(*parray));
+
+                if (array_size < 0) {
+                    return free_arrays(arrays, sequence_size);
+                }
+
+                if (array_size < min_array_size || min_array_size == 0) min_array_size = array_size;
+            }
+        } else {
+            for (Py_ssize_t i = 0; i < sequence_size; ++i, ++parray) {
+                fast_array = PySequence_Fast(*parray, "elements of input sequence must be sequence");
+                Py_ssize_t array_size = (fast_array == NULL) ? -1 : PySequence_Fast_GET_SIZE(fast_array);
+
+                if (array_size < 0) {
+                    Py_XDECREF(fast_array);
+                    return free_arrays(arrays, sequence_size);
+                }
+                Py_DECREF(array);
+                arrays[i] = fast_array;
+
+                if (array_size < min_array_size || min_array_size == 0) min_array_size = array_size;
+            }
         }
         npy_intp dims[1];
         dims[0] = min_array_size;
         PyArrayObject *result = (PyArrayObject*)PyArray_ZEROS(1, dims, NPY_OBJECT, 0);
         if (result == NULL) {
-            for (Py_ssize_t i = 0; i < sequence_size; ++i) Py_DECREF(arrays[i]);
-            free(arrays);
-            return NULL;
+            return free_arrays(arrays, sequence_size);
         }
 
         PyObject* separator = PyUnicode_FromFormat(" ");
         if (separator == NULL) {
-            for (Py_ssize_t i = 0; i < sequence_size; ++i) Py_DECREF(arrays[i]);
-            free(arrays);
             Py_DECREF(result);
-            return NULL;
+            return free_arrays(arrays, sequence_size);
         }
         PyObject* item = NULL;
-        PyObject* result_sequence = PyList_New(sequence_size);
+        PyObject* list_to_join = PyList_New(sequence_size);
         for (Py_ssize_t i = 0; i < min_array_size; ++i) {
-            for (Py_ssize_t j = 0; j < sequence_size; ++j) {
-                item = PySequence_Fast_GET_ITEM(arrays[j], i);
-                if (item == NULL) {
-                    Py_DECREF(result_sequence);
-                    Py_DECREF(result);
-                    for (Py_ssize_t i = 0; i < sequence_size; ++i) Py_DECREF(arrays[i]);
-                    free(arrays);
-                    return NULL;
+            parray = arrays;
+            if (all_numpy) {
+                for (Py_ssize_t j = 0; j < sequence_size; ++j, ++parray) {
+                    PyArrayObject* arr = (PyArrayObject*)(*parray);
+                    item = PyArray_GETITEM(arr, PyArray_GETPTR1(arr, i));
+                    if (item == NULL) {
+                        Py_DECREF(list_to_join);
+                        Py_DECREF(result);
+                        return free_arrays(arrays, sequence_size);
+                    }
+                    PyList_SetItem(list_to_join, j, item);
                 }
-                Py_INCREF(item);
-                PyList_SetItem(result_sequence, j, item);
+            } else {
+                for (Py_ssize_t j = 0; j < sequence_size; ++j, ++parray) {
+                    item = PySequence_Fast_GET_ITEM(*parray, i);
+                    if (item == NULL) {
+                        Py_DECREF(list_to_join);
+                        Py_DECREF(result);
+                        return free_arrays(arrays, sequence_size);
+                    }
+                    Py_INCREF(item);
+                    PyList_SetItem(list_to_join, j, item);
+                }
             }
-            PyObject* result_string = PyUnicode_Join(separator, result_sequence);
+            PyObject* result_string = PyUnicode_Join(separator, list_to_join);
             if (result_string == NULL) {
-                Py_DECREF(result_sequence);
+                Py_DECREF(list_to_join);
                 Py_DECREF(result);
-                for (Py_ssize_t i = 0; i < sequence_size; ++i) Py_DECREF(arrays[i]);
-                free(arrays);
-                return NULL;
+                return free_arrays(arrays, sequence_size);
             }
             if (PyArray_SETITEM(result, PyArray_GETPTR1(result, i), result_string) != 0) {
                 PyErr_SetString(PyExc_RuntimeError, "Cannot set resulting item");
-                Py_DECREF(result_sequence);
+                Py_DECREF(list_to_join);
                 Py_DECREF(result);
                 Py_DECREF(result_string);
-                for (Py_ssize_t i = 0; i < sequence_size; ++i) Py_DECREF(arrays[i]);
-                free(arrays);
-                return NULL;
+                return free_arrays(arrays, sequence_size);
             }
             Py_DECREF(result_string);
         }
-        Py_DECREF(result_sequence);
-        for (Py_ssize_t i = 0; i < sequence_size; ++i) Py_DECREF(arrays[i]);
-        free(arrays);
+        Py_DECREF(list_to_join);
+        (void)free_arrays(arrays, sequence_size);
         return (PyObject*)result;
     }
 
